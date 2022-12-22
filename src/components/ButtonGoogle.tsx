ee93880b3fb66a51f5687b25e3fbbf4ec327f30f
hilmimarifin <hilmi.m.arifin@gmail.com>
2022-12-22 21:19:29 +0700

    add signin google button

---
+++ src/components/ButtonGoogle.tsx
@@ -0,0 +1,70 @@
+import axios from "axios";
+import { useEffect, useRef, useState } from "react";
+
+export default function GoogleButton(): JSX.Element {
+    const clientId: any = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID
+    const [loading, setLoading] = useState(false);
+    const [scriptLoaded, setScriptLoaded] = useState(false);
+
+    const handleGoogle = (res: any) => {
+        setLoading(true)
+        fetch("http://localhost:4000/user/signup-google", {
+            // fetch("http://localhost:5152/signup", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+
+            body: JSON.stringify({ credential: res.credential }),
+        })
+            .then((res) => {
+                setLoading(false)
+                return res.json();
+            })
+            .then((data) => {
+                if (data?.user) {
+                    localStorage.setItem("user", JSON.stringify(data?.user));
+                    window.location.reload();
+                }
+
+                throw new Error(data?.message || data);
+            })
+            .catch((error) => {
+                console.log(error?.message);
+            });
+    }
+    useEffect(() => {
+        /* global google */
+        console.log('windows google', window.google);
+        const initializeGoogle = () => {
+            if (!window.google || scriptLoaded) return;
+        
+            setScriptLoaded(true);
+            window.google.accounts.id.initialize({
+                client_id: clientId,
+                callback: handleGoogle,
+            });
+
+            window.google.accounts.id.renderButton(document.getElementById("signUpDiv") as HTMLElement, {
+                type: "standard",
+                theme: "filled_black",
+                // size: "small",
+                text: "continue_with",
+                shape: "pill",
+            });
+
+        }
+
+            const script = document.createElement("script");
+            script.src = "https://accounts.google.com/gsi/client";
+            script.onload = initializeGoogle;
+            script.async = true;
+            script.id = "google-client-script";
+            document.querySelector("body")?.appendChild(script);
+
+            // google.accounts.id.prompt()
+    }, [handleGoogle, scriptLoaded]);
+
+
+    return loading ? <div>Loaing....</div> : <div id="signUpDiv" data-text="signup_with"></div>;
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,10 @@
 import '../../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <>
+    <Script type="application/javascript" src="https://accounts.google.com/gsi/client" />
+    <Component {...pageProps} />
+  </>
 }
