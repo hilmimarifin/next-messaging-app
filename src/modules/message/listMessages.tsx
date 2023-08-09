544e2057d18f425cd9d401dc5093dc32ab9f6da8
Hilmi <hilmi.m.arifin@gmail.com>
2023-08-09 11:44:08 +0700

    feat: implement socket io client for list message

--- src/modules/message/listMessages.tsx
+++ src/modules/message/listMessages.tsx
@@ -1,30 +1,25 @@
 import React from 'react'
 import MessageItem from './messageItem'
 import { IMessage } from '../../types/message'
-import { useQuery } from 'react-query'
+import { useQuery, useQueryClient } from 'react-query'
 import { getListMessage } from '../../services/message'
-
+import { io } from "socket.io-client";
 
 type Props = {}
-const dataDummy: IMessage[] = []
-for (let i = 1; i <= 10; i++) {
-    dataDummy.push({
-        receiverName: `User ${i}`,
-        receiverId: i.toString(),
-        text: `This is meessage from user ${i} fasdfjksdajfsdhf akshfasldf jaskjdfljalsjdlkfj asdfjkljasdfjsdf alsjdflk`,
-        unread: i
-    })
-}
 const ListMessages = (props: Props) => {
+    const queryClient = useQueryClient()
     const { data, isError, isLoading } = useQuery('Fetch list Message', getListMessage)
-    console.log('list of data', data);
-
+    const socket = io('http://localhost:5001');
+    socket.on("messageSent", (arg) => {
+        console.log(arg);
+        queryClient.invalidateQueries("Fetch list Message")
+      });
     return (
-        <div>
+        <div className=' w-[425px] m-auto'>
             {isLoading ? <div>Loading... </div> : 
                 data?.data?.map((message: IMessage) =>
                 <li className=' list-none' key={message.receiverId}>
-                    <MessageItem  receiverName={message.receiverName} receiverId={message.receiverId} text={message.text} unread={message.unread} />
+                    <MessageItem  receiverName={message.receiverName} receiverId={message.receiverId} text={message.text} unread={message.unread} createdAt={message.createdAt}/>
                 </li>
             )}
         </div>
