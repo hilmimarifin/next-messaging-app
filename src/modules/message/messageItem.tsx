be428905ffc2ff12230a72db39e9be7cbec6bdfd
Hilmi <hilmi.m.arifin@gmail.com>
2023-08-09 11:43:22 +0700

    feat: add mesage item for list message

--- src/modules/message/messageItem.tsx
+++ src/modules/message/messageItem.tsx
@@ -6,16 +6,32 @@ import Link from 'next/link'
 
 const MessageItem = (props: IMessage) => {
     return (
-        <div className=' p-4 border-b-2'>
-            <Link  href={`messages/${props.receiverId}`}>
-                <div>
-                    <span className=' font-bold'>{props.receiverName}</span>
+        <Link href={`messages/${props.receiverId}`}>
+            <div className='flex flex-row justify-between p-4 border-b-2'>
+                <div className=' flex flex-row'>
+                    <div className="chat-image avatar">
+                        <div className="w-10 rounded-full">
+                            <img src="https://placeimg.com/192/192/people" />
+                        </div>
+                    </div>
+                    <div className=' flex flex-col justify-between ml-4'>
+                        <span className=' font-bold'>{props.receiverName}</span>
+                        <p>{props.text}</p>
+                    </div>
                 </div>
-                <div>
-                    <p>{props.text}</p>
+                <div className=' flex flex-col justify-between'>
+                    <div>
+                        <time className="text-xs opacity-50">{new Date(props.createdAt).toLocaleString()}</time>
+                    </div>
+                    <div className='flex flex-row-reverse'>
+                        {
+                            props.unread? <div className=' badge badge-primary'>{props.unread}</div> : <></>
+                        }
+                    </div>
                 </div>
-            </Link>
-        </div>
+            </div>
+
+        </Link>
     )
 }
 
